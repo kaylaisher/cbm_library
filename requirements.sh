a992ccd914230381f09a56b4d2f2149eb9f18b43
kaylaisher <147053990+kaylaisher@users.noreply.github.com>
2025-08-27 18:19:52 -0700

    Update package versions in requirements.sh
    Updated package versions in requirements.sh for consistency and compatibility.

--- requirements.sh
+++ requirements.sh
@@ -1,11 +1,12 @@
-pip install torch
-pip install matplotlib >= 3.5.1
-pip install torchvision >= 0.13
+pip install torch==2.2.1
+pip install matplotlib == 3.9.0
+pip install torchvision ==0.17.1
+pip install torchaudio==2.2.1
 pip install gdown >= 4.7.1
 pip install ftfy >= 6.1.1
 pip install regex >= 2022.10.31
-pip install tqdm >= 4.64.1
-pip install pandas >= 1.5.3
+pip install tqdm ==4.66.4
+pip install pandas ==2.2.2
 pip install pytorchcv >= 0.0.67
 pip install sentence_transformers >= 2.2.2
 pip install jupyter
@@ -18,3 +19,12 @@ pip install "git+https://github.com/openai/CLIP.git"
 pip install open-clip-torch
 pip install glm-saga
 pip install sentence-transformers
+pip install tensorboard==2.17.1
+pip install loguru
+pip install numpy==1.26.4
+pip install Pillow==10.4.0
+pip install opencv-python-headless==4.10.0.84
+pip install timm==0.9.16
+pip install einops==0.8.0
+pip install scikit-learn==1.5.2
+pip install pyyaml==6.0.2
