bb2748b2f760f2d2f5d51cf0b851468c88e53c04
kaylaisher <147053990+kaylaisher@users.noreply.github.com>
2025-08-27 17:40:27 -0700

    Add FinalLayerMethod and UnifiedFinalTrainer to __all__

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 from .lf_cbm import LabelFreeCBM, read_concepts_file
 from .vlg_cbm import VLGCBM
+from .final_layer import FinalLayerMethod, UnifiedFinalTrainer
 
 
-__all__ = ["LabelFreeCBM", "VLGCBM", "read_concepts_file"]
+
+__all__ = ["LabelFreeCBM", "VLGCBM", "read_concepts_file", "FinalLayerMethod", "UnifiedFinalTrainer"]
